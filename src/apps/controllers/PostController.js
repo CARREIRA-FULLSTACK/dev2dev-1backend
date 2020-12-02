5c9731b1713deccb2e0256f52448db54cff8a88b
danieldeandradelopes@gmail.com <danieldeandradelopes@gmail.com>
2020-12-02 07:40:15 -0300

    added upload

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -6,6 +6,22 @@ class PostController {
 
     return res.status(200).json(insertedPost);
   }
+
+  async upload(req, res) {
+    const { filename } = req.file;
+
+    return res.send({ image: `uploads/${filename}` });
+  }
+
+  async getPosts(req, res) {
+    const allPosts = await Post.findAll();
+
+    if (!allPosts) {
+      return res.send({ message: "Posts not found" });
+    }
+
+    return res.status(200).json(allPosts);
+  }
 }
 
 module.exports = new PostController();
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,19 @@
 const { Router } = require("express");
 const PostController = require("./apps/controllers/PostController");
+const multer = require("multer");
+
+const { v4 } = require("uuid");
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename(req, file, callback) {
+      const filename = `${v4()}-${file.originalname}`;
+
+      return callback(null, filename);
+    },
+  }),
+});
 
 const routes = new Router();
 
@@ -8,5 +22,9 @@ routes.get("/health", (req, res) => {
 });
 
 routes.post("/post", PostController.store);
+
+routes.get("/post", PostController.getPosts);
+
+routes.put("/upload", upload.single("image"), PostController.upload);
 
 module.exports = routes;
