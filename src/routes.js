b05938de4bfa5ca8bc6004ae28a24c9440496a74
danieldeandradelopes@gmail.com <danieldeandradelopes@gmail.com>
2020-12-02 06:18:20 -0300

    created upload configs

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,21 @@
 const { Router } = require("express");
 const PostController = require("./apps/controllers/PostController");
 
+const multer = require("multer");
+
+const { uuid } = require("uuidv4");
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename(req, file, callback) {
+      const fileName = `${uuid()}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+});
+
 const routes = new Router();
 
 routes.get("/health", (req, res) => {
@@ -8,5 +23,11 @@ routes.get("/health", (req, res) => {
 });
 
 routes.post("/post", PostController.store);
+
+routes.post("/avatar", upload.single("avatar"), function (req, res) {
+  const { filename, size } = req.file;
+
+  return res.send({ image: `/uploads/${filename}`, size });
+});
 
 module.exports = routes;
